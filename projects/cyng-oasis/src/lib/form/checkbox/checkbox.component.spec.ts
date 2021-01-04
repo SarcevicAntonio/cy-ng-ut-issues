24653904a2c2aa7ca5f9d77681d12b8b07535181
Antonio Sarcevic <sarcevic@westphalia-datalab.com>
2021-01-04 08:43:18 +0100

    tried loading external styles

--- projects/cyng-oasis/src/lib/form/checkbox/checkbox.component.spec.ts
+++ projects/cyng-oasis/src/lib/form/checkbox/checkbox.component.spec.ts
@@ -3,11 +3,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TranslateModule } from '@ngx-translate/core';
-import { initEnvHtml, mountHtml } from 'cypress-angular-unit-test';
+import { initEnvHtml, mountHtml, setConfig } from 'cypress-angular-unit-test';
 import { CheckboxComponent } from './checkbox.component';
 
 describe('CheckboxComponent', () => {
   it('shows the input', () => {
+    setConfig({ cssFile: 'node_modules/@angular/material/prebuilt-themes/indigo-pink.css', });
     // Init Angular stuff
     initEnvHtml(CheckboxComponent, {
       imports: [
@@ -20,9 +21,10 @@ describe('CheckboxComponent', () => {
       ]
     });
     // component + any inputs object
-    mountHtml(`
+    const fixture = mountHtml(`
     <cyng-checkbox>Hello World</cyng-checkbox>
     `);
+    fixture.detectChanges()
     // mount(CheckboxComponent, { title: 'Hello World' });
     // use any Cypress command afterwards
     cy.contains('Hello World');
